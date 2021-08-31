e5b59390ff14377782a10d1ea4b8afa7d6ea29a6
Evaldas Jocys <evaldas@jocys.com>
2021-08-31 11:41:04 +0100

    Clear builds script updated.

--- Clear_Builds.ps1
+++ Clear_Builds.ps1
@@ -1,46 +1,48 @@
-<#
-.SYNOPSIS
-    Removes temporary bin and obj folders.
-.NOTES
-    Author:     Evaldas Jocys <evaldas@jocys.com>
-    Modified:   2021-04-14
-.LINK
-    http://www.jocys.com
-#>
+# Removes temporary bin and obj folders.
 # ----------------------------------------------------------------------------
 # Get current command path.
-[string]$current = $MyInvocation.MyCommand.Path;
+[string]$current = $MyInvocation.MyCommand.Path
 # Get calling command path.
-[string]$calling = @(Get-PSCallStack)[1].InvocationInfo.MyCommand.Path;
+[string]$calling = @(Get-PSCallStack)[1].InvocationInfo.MyCommand.Path
 # If executed directly then...
 if ($calling -ne "") {
-    $current = $calling;
+    $current = $calling
 }
-$file = Get-Item $current;
+
+$file = Get-Item $current
 # Working folder.
 $wdir = $file.Directory.FullName;
+
 # ----------------------------------------------------------------------------
+
+$global:removeCount = 0;
+$global:skipCount = 0;
+
 Function RemoveDirectories
 {
 	# Parameters.
-	param ($pattern);
+	Param ($pattern)
 	# Function.
-	[System.IO.DirectoryInfo[]]$items = Get-ChildItem $wdir -Filter $pattern -Recurse -Force | Where-Object {$_ -is [System.IO.DirectoryInfo]};
+	$items = Get-ChildItem $wdir -Filter $pattern -Recurse -Force | Where-Object {$_ -is [System.IO.DirectoryInfo]};
 	foreach ($item in $items)
 	{
-		$item.Refresh();
-		if ($item.Exists -eq $false){
-			continue;
+		[System.IO.DirectoryInfo] $parent = $item.Parent;
+		$projects = $parent.GetFiles("*.*proj", [System.IO.SearchOption]::TopDirectoryOnly);
+		# If Project file in parent folder was found then...
+		if ($projects.length -gt 0){
+			Write-Output "Remove: $($item.FullName)";
+			$global:removeCount += 1;
+			Remove-Item -LiteralPath $item.FullName -Force -Recurse
 		}
-		# If folder is not inside the project folder then continue.
-		if ($item.Parent.GetFiles("*.*proj").Length -eq 0){
-			continue;
+		else
+		{
+			Write-Output "Skip:   $($item.FullName)";
+			$global:skipCount += 1;
 		}
-		Write-Output $item.FullName;
-		Remove-Item -LiteralPath $item.FullName -Force -Recurse;
 	}
 }
-# Remove 'obj' folders first, because it can contain 'bin' inside.
-RemoveDirectories "obj";
-RemoveDirectories "bin";
-pause;
+# Clear directories.
+RemoveDirectories "bin"
+RemoveDirectories "obj"
+Write-Output "Skipped: $global:skipCount, Removed: $global:removeCount";
+pause
