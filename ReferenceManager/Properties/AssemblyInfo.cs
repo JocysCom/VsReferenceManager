2f73fc6e7b48ded625930fc7509462954e519bbb
Evaldas Jocys <evaldas@jocys.com>
2021-09-10 18:31:33 +0100

    Description update

--- ReferenceManager/Properties/AssemblyInfo.cs
+++ ReferenceManager/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Jocys.com VS Reference Manager")]
-[assembly: AssemblyDescription("Update DLL references with project references.")]
+[assembly: AssemblyDescription("Scan and update DLL references with Project references.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jocys.com")]
 [assembly: AssemblyProduct("VS Reference Manager")]
